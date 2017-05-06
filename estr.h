0f25b7efcc083ed4c7bce568f8462c706c355d1d
ibroheem <movichtech@gmail.com>
2017-05-07 00:35:19 +0100

    Added Project Files

---
+++ estr.h
@@ -0,0 +1,40 @@
+#ifndef estr_h
+#define estr_h
+
+namespace utils
+{
+   template <unsigned int CAPACITY, class T = char>
+   class estr
+   {
+      public:
+         T* begin() { return &m_data[0]; }
+         T* cbegin() const { return begin(); }
+
+         T* end() { return &m_data[length()]; }
+         T* cend() const  { return end(); }
+
+         /** Note: Erasure is typically over writing with '\0' */
+         void erase(T* str, unsigned int index);
+
+         size_t capacity() const { return CAPACITY; }
+         size_t length() const;
+         size_t size() const;
+
+         template <class CT>
+         int index_of(CT);
+
+         bool operator<(const estr&);
+         bool operator==(const estr&);
+         bool operator!=(const estr&);
+
+      private:
+         T m_data[CAPACITY];
+   };
+
+   void usage()
+   {
+      estr<20> str;
+   }
+}
+
+#endif//estr_h
